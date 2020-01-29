3cd383bc4e82560be47228f22932d3a099fabec7
Derrik Campbell <derrikcampbell@hotmail.com>
2020-01-29 08:44:40 -0500

    adding socket stuff

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 
+// import socket.io
+const io = require('socket.io')(); // instantiate the socket.io library right away with the () method -> makes it run
+
 const port = process.env.PORT || 3030;
 
-// tell express where our static files are (js, images, css etc)
+// this tells express where static files are js, images, css 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -13,3 +16,30 @@ app.get('/', (req, res) => {
 const server = app.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });
+
+// this is for all of our socket.io messaging 
+
+// attach socket.io
+io.attach(server);
+
+io.on('connection', function(socket){  //socket is your connection
+    console.log('user connected');
+    socket.emit('connected', { sID: `${socket.id}`, message: 'new connection' });
+
+   
+
+    socket.on('chat_message', function(msg) {
+        console.log(msg);
+
+    
+        io.emit('new_message', { id: socket.id, message: msg })
+    })
+
+    //listen for a disconnect event
+    socket.on('disconnect', function() {
+        console.log('a user disconnected');
+
+        message = `${socket.id} has left the chat!`;
+        io.emit('user_disconnect', message);
+    })
+})
--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,1 +1,17 @@
 // imports always go first - if we're importing anything
+
+const socket = io();
+
+//packet is the data sent through the server with the connect event
+
+function setUserId(packet) {
+    // debugger;
+    console.log(packet);
+}
+
+function showDisconnectMessage(){
+    console.log('a user disconnected');
+}
+
+socket.addEventListener('connected', setUserId);
+socket.addEventListener('disconnect', showDisconnectMessage)
